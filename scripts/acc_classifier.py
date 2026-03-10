15f89faee71192e2f946c7b8c5062c68afcb5452
wildcatshawkeyesgh <wildcatshawkeyesgh@gmail.com>
2026-03-09 21:48:17 -0500

    optimus_debug2

--- scripts/acc_classifier.py
+++ scripts/acc_classifier.py
@@ -22,7 +22,7 @@ print(device)
 device_id = deepl.get_best_gpu(strategy="utilization")
 device = torch.device(f"cuda:{device_id}")
 print(f"Selected GPU: {device_id}")
-eta = 0.0003
+eta = 0.001
 epoch = 200
 patience = 7
 batch_size = 1024
